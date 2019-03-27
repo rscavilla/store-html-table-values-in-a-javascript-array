d15c07f2139c74c1a1f2c4a0fa7a570328cc9568
Robert Scavilla <bob@fourfront.us>
2019-03-27 16:18:52 -0400

    Create processJSONArray.php

---
+++ processJSONArray.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Sample PHP code
+ * Input: POST json array
+ * return: PHP array
+ * 
+ * Robert Scavilla
+ * https://github.com/rscavilla/store-html-table-values-in-a-javascript-array/new/master
+ */
+echo processJSONArray();
+
+function processJSONArray()
+{
+    $tableData = stripcslashes($_POST['pTableData']);
+    $tableData = json_decode($tableData,TRUE);
+    return '$tableData[0][\'description\'] = ' . $tableData[0]['description'] . "\n"
+            . '$tableData[1][\'description\'] = ' . $tableData[1]['description'] . "\n"
+            . '$tableData[2][\'description\'] = ' .  $tableData[2]['description'] . "\n\n"
+            . print_r($tableData, true);
+}
+
+?>
